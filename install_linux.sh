e61d88f9fb7234f59dd7243520a828f8999c0f09
Artem Chistyakov <chistyakov.artem@gmail.com>
2016-06-23 10:54:03 -0400

    Add git-web-open script.

--- install_linux.sh
+++ install_linux.sh
@@ -73,6 +73,7 @@ mkdir -p ~/bin
 ln -sf $DIR/scripts/pbcopy ~/bin/
 ln -sf $DIR/scripts/impostor ~/bin/
 ln -sf $DIR/scripts/dwmdwmdwm ~/bin/
+ln -sf $DIR/scripts/git-web-open ~/bin/
 ln -sf $DIR/../scripts/git-web ~/bin/
 
 ################################################################################
