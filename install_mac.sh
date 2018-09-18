69d1aba9ca8b18113b635c1c5848d3edd73b3ef2
Artem Chistyakov <tema@wildbit.com>
2018-09-18 10:17:08 -0400

    Add a function to fuzzy-search projects.

--- install_mac.sh
+++ install_mac.sh
@@ -14,3 +14,17 @@ ln -sf $DIR/scripts/git-web ~/bin/
 ln -sf $DIR/mac/scripts/git-web-open ~/bin/
 ln -sf $DIR/mac/scripts/appify ~/bin/
 
+echo "Making sure Homebrew is installed..."
+brew --version > /dev/null || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew install fzf
+
+echo "Including .bash_profile"
+include_string="source $DIR/mac/.bash_profile"
+
+if ! grep "^${include_string}$" ~/.bash_profile 2>&1 > /dev/null; then
+    echo "Adding the following string to ~/.bash_profile:"
+    echo "  ${include_string}"
+    echo $include_string >> ~/.bash_profile
+fi
+
---
+++ mac/.bash_profile
@@ -0,0 +1,13 @@
+p() {
+    cd ~/projects/$(ls ~/projects | fzf)
+}
+
+bind '"\C-p":"p\n"'
+
+if [ -f "$(brew --prefix)/opt/bash-git-prompt/share/gitprompt.sh" ]; then
+    export GIT_PROMPT_THEME=Solarized
+    export GIT_PROMPT_END=" ${White}${ResetColor}$ "
+    __GIT_PROMPT_DIR=$(brew --prefix)/opt/bash-git-prompt/share
+    source "$(brew --prefix)/opt/bash-git-prompt/share/gitprompt.sh"
+fi
+
